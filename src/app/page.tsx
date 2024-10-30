6f1ae29c8f630f2ae78a190ac1f85e792422dbc2
Nicholas Robert Beckham <nibih@live.com>
2024-10-30 11:42:44 +0700

    Refactor layout and add registration functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,30 @@
 "use client";
-import { useState } from "react";
-import { encryptPin } from "../lib/encryption";
-
+import { useEffect } from "react";
+import { useRegister } from "@/context/RegisterContext";
+import { useRouter } from "next/navigation";
 export default function Register() {
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [otpCode, setOtpCode] = useState("");
-  const [pin, setPin] = useState("");
-  const [confirmPin, setConfirmPin] = useState("");
-
-  const handleRegister = () => {
-    if (pin === confirmPin) {
-      const encryptedPin = encryptPin(pin);
-      console.log(
-        "Phone:",
-        phoneNumber,
-        "OTP:",
-        otpCode,
-        "Encrypted PIN:",
-        encryptedPin
-      );
-      alert("Registration successful! Your data has been securely encrypted.");
-    } else {
-      alert("Pins don't match!");
+  // mock, so phone numbner is stored, otp is just a value that is stored
+  // after phone is filled and button is pressed, load otp component
+  const { stage } = useRegister();
+  const router = useRouter();
+  useEffect(() => {
+    if (stage === "phone") {
+      router.push("/phone");
     }
-  };
-
+    if (stage === "otp") {
+      router.push("/otp");
+    }
+    if (stage === "register") {
+      router.push("/register");
+    }
+    if (stage === "pin") {
+      router.push("/pin");
+    }
+  }, [stage]);
   return (
-    // orange red bg
-    // makeheight fill screen
-    <div className="bg-white p-6 h-full mx-auto mt-10 rounded-t-lg">
-      <div
-        className="h-full m-auto
-      w-96
-      flex flex-col"
-      >
-        {/* logo lrt */}
-        <img src="/Logo_LRT.png" alt="Logo" className="h-24 mx-auto mb-4" />
-        <h2 className="text-2xl font-semibold text-center mb-4">Register</h2>
-
-        <div className="flex flex-col">
-          <label className="text-sm font-medium">Nomor Telepon</label>
-          <input
-            type="tel"
-            placeholder="Masukkan nomor telepon"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            className="p-2 border rounded mt-1"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label className="text-sm font-medium">Kode OTP</label>
-          <input
-            type="text"
-            placeholder="Masukkan kode OTP"
-            value={otpCode}
-            onChange={(e) => setOtpCode(e.target.value)}
-            className="p-2 border rounded mt-1"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label className="text-sm font-medium">Create PIN</label>
-          <input
-            type="password"
-            placeholder="Buat PIN"
-            value={pin}
-            onChange={(e) => setPin(e.target.value)}
-            className="p-2 border rounded mt-1"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label className="text-sm font-medium">Confirm PIN</label>
-          <input
-            type="password"
-            placeholder="Konfirmasi PIN"
-            value={confirmPin}
-            onChange={(e) => setConfirmPin(e.target.value)}
-            className="p-2 border rounded mt-1"
-          />
-        </div>
-
-        <button
-          onClick={handleRegister}
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Daftar
-        </button>
-      </div>
-    </div>
+    //load pin component
+    <>
+      <h1 className="text-2xl">LRT X JakOne Pay</h1>
+    </>
   );
 }
---
+++ src/context/RegisterContext.tsx
@@ -0,0 +1,56 @@
+// context/RegisterContext.js
+"use client";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+const RegisterContext = createContext({
+  phoneNumber: "",
+  setPhoneNumber: (phoneNumber: string) => {},
+  otp: "",
+  setOtp: (otp: string) => {},
+  stage: "phone",
+  setStage: (stage: string) => {},
+  encryptedPin: "",
+  setEncryptedPin: (encryptedPin: string) => {},
+});
+
+interface RegisterProviderProps {
+  children: ReactNode;
+}
+
+export const RegisterProvider = ({ children }: RegisterProviderProps) => {
+  // phone number and stage of registration
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const [stage, setStage] = useState("phone");
+  const [encryptedPin, setEncryptedPin] = useState("");
+  useEffect(() => {
+    // console log state changes
+    console.log(phoneNumber);
+    console.log(otp);
+    console.log(stage);
+  }, [phoneNumber, otp, stage]);
+  return (
+    <RegisterContext.Provider
+      value={{
+        phoneNumber,
+        setPhoneNumber,
+        otp,
+        setOtp,
+        stage,
+        setStage,
+        encryptedPin,
+        setEncryptedPin,
+      }}
+    >
+      {children}
+    </RegisterContext.Provider>
+  );
+};
+
+// Custom hook for easier usage
+export const useRegister = () => useContext(RegisterContext);
